437bab4976b3b9e63981c48230f193dbeab19d8d
Jake Dube <jake4pro@gmail.com>
2016-11-15 22:59:38 -0500

    Initial commit.

---
+++ uv_map_tools.py
@@ -0,0 +1,108 @@
+import bpy
+from bpy.types import Operator
+
+bl_info = {
+    "name": "UV Tools",
+    "author": "Jake Dube",
+    "version": (1, 0),
+    "blender": (2, 77, 0),
+    "location": "UV maps in properties window",
+    "description": "Some tools for uv maps that should already be in Blender.",
+    "category": "UV"}
+
+
+def make_active(name):
+    uvs = bpy.context.scene.objects.active.data.uv_textures
+    for uv in uvs:
+        if uv.name == name:
+            uvs.active = uv
+            return
+    print("Could not find:", name, "\n(this should never happen)")
+
+
+def move_to_bottom(index):
+    uvs = bpy.context.scene.objects.active.data.uv_textures
+    uvs.active_index = index
+    new_name = uvs.active.name
+
+    bpy.ops.mesh.uv_texture_add()
+
+    # delete the "old" one
+    make_active(new_name)
+    bpy.ops.mesh.uv_texture_remove()
+
+    # set the name of the last one
+    uvs.active_index = len(uvs) - 1
+    uvs.active.name = new_name
+
+
+class MoveUVMapDown(Operator):
+    bl_idname = "uv_tools.move_uvmap_down"
+    bl_label = "Move Down"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        uvs = context.scene.objects.active.data.uv_textures
+
+        # get the selected UV map
+        orig_ind = uvs.active_index
+        orig_name = uvs.active.name
+
+        if orig_ind == len(uvs) - 1:
+            return {'FINISHED'}
+
+        # use "trick" on the one after it
+        move_to_bottom(orig_ind + 1)
+
+        # use the "trick" on the UV map
+        move_to_bottom(orig_ind)
+
+        # use the "trick" on the rest that are after where it was
+        for i in range(orig_ind, len(uvs) - 2):
+            move_to_bottom(orig_ind)
+
+        make_active(orig_name)
+
+        return {'FINISHED'}
+
+
+class MoveUVMapUp(Operator):
+    bl_idname = "uv_tools.move_uvmap_up"
+    bl_label = "Move Up"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        uvs = context.scene.objects.active.data.uv_textures
+
+        if uvs.active_index == 0:
+            return {'FINISHED'}
+
+        original = uvs.active.name
+        uvs.active_index -= 1
+        bpy.ops.uv_tools.move_uvmap_down()
+        make_active(original)
+
+        return {'FINISHED'}
+
+
+def uv_tools_addition(self, context):
+    layout = self.layout
+    col = layout.column(align=True)
+    col.operator("uv_tools.move_uvmap_up", icon='TRIA_UP')
+    col.operator("uv_tools.move_uvmap_down", icon='TRIA_DOWN')
+
+
+def register():
+    bpy.utils.register_class(MoveUVMapDown)
+    bpy.utils.register_class(MoveUVMapUp)
+    bpy.types.DATA_PT_uv_texture.append(uv_tools_addition)
+
+
+def unregister():
+    bpy.utils.unregister_class(MoveUVMapDown)
+    bpy.utils.unregister_class(MoveUVMapUp)
+    bpy.types.DATA_PT_uv_texture.remove(uv_tools_addition)
+
+
+if __name__ == "__main__":
+    register()
